51a29ce8788d48a47600f8d56d50a54532d0e897
sonjastndl <steindl.sonja@gmail.com>
2023-06-15 14:36:11 +0200

    get

--- projects/WormPicker/scripts/GetDataNew.py
+++ projects/WormPicker/scripts/GetDataNew.py
@@ -7,6 +7,7 @@ from dateutil import parser
 from datetime import datetime
 import math
 from osgeo import gdal, osr
+import os
 
 from module_crs_converter import trans4m2wgs84, trans4mfromwgs84
 
@@ -39,6 +40,7 @@ end_result=[]
 fully_covered=[]
 covered_spatially=[]
 dropout=[]
+layerlist=["sampleID"]
 with open("/media/ssteindl/fairicube/uc3/uc3-drosophola-genetics/projects/WormPicker/output/coveredsamples_wcs.csv", newline='') as csvfile:
     sample_reader = csv.DictReader(csvfile)
     for s_row in sample_reader:
@@ -50,6 +52,7 @@ with open("/media/ssteindl/fairicube/uc3/uc3-drosophola-genetics/projects/WormPi
         #print("ORIGINAL COORDINATES OF SAMPLE\n:", name_date, latitude_original, longitude_original, "DATE:", time_asked)
         #datetime3 = parser.isoparse(time_asked)
         sample_result=[]
+        sample_result.append(name_date)
         #print(datetime3)
         layer=None
         converted_date=None
@@ -72,6 +75,7 @@ with open("/media/ssteindl/fairicube/uc3/uc3-drosophola-genetics/projects/WormPi
                 if any(value == "NA" for value in row.values()):
                     continue
                 layer=row["CoverageID"]
+                layerlist.append(layer)
                 #print(layer)
                 crs=row["CRS"]
                 resolution=0
@@ -82,9 +86,7 @@ with open("/media/ssteindl/fairicube/uc3/uc3-drosophola-genetics/projects/WormPi
                 #print("LIMITS: ", time_min, time_max, "MI\n", row["minlat"], row["maxlat"] )
                 datetime1 = parser.isoparse(time_min)
                 datetime2 = parser.isoparse(time_max)
-                #print(layer, crs, lat, lon, resolution)
-                #print(layer, search_time, converted_date, datetime1, datetime2) 
-                #print(lat,lon)
+                #print(layer, crs,resolution)
                 strlat="&subset=Y({},{})"
                 strlon = "&subset=X({},{})"
                 #print("SUBSETS DEFAULT",strlon,strlat)
@@ -113,37 +115,47 @@ with open("/media/ssteindl/fairicube/uc3/uc3-drosophola-genetics/projects/WormPi
                     longitude_request=longitude_request
                     strlat="&subset=Lat({},{})"
                     strlon = "&subset=Long({},{})"
-                    print(crs,latitude_request,longitude_request)
+                    #print(crs,longitude_request,latitude_request)
                 else:
                     crs_indicator= crs.replace("EPSG/0/", "EPSG:")
-                    longitude_request,latitude_request= trans4mfromwgs84(crs_indicator,float(longitude_request), float(latitude_request))
+                    longitude_request,latitude_request= trans4mEPSG("EPSG:4326",crs_indicator,float(longitude_request), float(latitude_request))
+                    #print(longitude_request,latitude_request)
                 #print(strlat, strlon)
                 #coverage_id = layer
                 x1= float(latitude_request)
-                x2= x1 + resolution
+                #x2= x1 + resolution
+                x2=x1
                 #print(x1,x2, resolution)
                 #strlat="&subset=Lat({},{})"
                 subset_lat = strlat.format(x1,x2)
                 y1= float(longitude_request)
-                y2= y1 + resolution
+                y2=y1
+                #y2= y1 + resolution
                 #strlon = "&subset=Long({},{})"
                 subset_long= strlon.format(y1,y2)
                 #print("CRS:",crs, "RESOLUTION:", resolution, subset_long, subset_lat)
                 request_cov_id = "&COVERAGEID=" + layer
                 request_encode_format = "&FORMAT=text/csv"
-                #print(layer,lat,lon, row["minlat"], row["maxlat"])
                 if float(latitude_request) > float(row["minlat"]) and float(latitude_request) < float(row["maxlat"]) and float(longitude_request) > float(row["minlong"]) and float(longitude_request) < float(row["maxlong"] ):
                     my_dict[name_date]=set()
                     my_dict[name_date].add(layer)  ##this saves information on which
                     response = requests.get(
                     base_wcs_url + "&request=GetCoverage" + request_cov_id + subset_ansi + subset_lat + subset_long + request_encode_format,auth=(rasdaman_username, rasdaman_password), verify=False)
-                    sample_result.append((layer,response.text))
+                    valls=(str(response.text)[1:-1])
+                    sample_result.append(valls)
                 else:
-                    print("YAY")
-                    dropout.append(("ERROR: Sample " + str(name_date) + " not covered in area of " + layer + ". You calculated EPSG for WGS84:" + str(latitude_request) + str(longitude_request) + ". Boundaries lat are" + row["minlat"]+ row["maxlat"] + "long: " + row["minlong"] + row["maxlong"]))
+                    #print("YAY")
+                    valls=("NA")
+                    sample_result.append(valls)
+                    #dropout.append(("ERROR: Sample " + str(name_date) + " not covered in area of " + layer + ". You calculated EPSG for WGS84:" + str(latitude_request) + str(longitude_request) + ". Boundaries lat are" + row["minlat"]+ row["maxlat"] + "long: " + row["minlong"] + row["maxlong"]))
         print(sample_result)
-        end_result.append((s_row["sampleId"], sample_result))
+        end_result.append((sample_result))
     print(end_result)
+
+with open("/media/ssteindl/fairicube/uc3/uc3-drosophola-genetics/projects/WormPicker/output/layerdata.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(layerlist)
+    writer.writerows(end_result)
 
 
                 ##add metadata link for description of the layer data
